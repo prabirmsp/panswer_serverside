da33b381e0cdfc762f15910cab642748713227ba
Prabir Pradhan <prabirmsp@gmail.com>
2015-06-26 11:46:06 +0545

    initial

--- paJSON.php
+++ paJSON.php
@@ -14,7 +14,7 @@ $JSONData = new stdClass (); // class for JSONData
 if (isset ( $_GET ['get_items_from_dspace_id'] )) { // field set in URL
 	$dspace_id = $_GET ['get_items_from_dspace_id'];
 	
-	$query_str = 'SELECT * FROM ans_dspace_item WHERE collection_id = ' . $dspace_id;
+	$query_str = "SELECT * FROM ans_dspace_item WHERE collection_id = '" . $dspace_id . "' ORDER BY title";
 	$items = array ();
 	if ($result = mysqli_query ( $link, $query_str )) { // query for items
 		
@@ -36,6 +36,7 @@ if (isset ( $_GET ['get_items_from_dspace_id'] )) { // field set in URL
 							$document_href = $bitstream_row ['href'];
 							$document_thumb_href = $bitstream_row ['thumb_href'];
 							$document_size = $bitstream_row ['size'];
+							$type = $bitstream_row ['type'];
 					}
 				}
 			} else {
@@ -45,17 +46,17 @@ if (isset ( $_GET ['get_items_from_dspace_id'] )) { // field set in URL
 			$items [] = array (
 					'item_dspace_id' => $item_dspace_id,
 					'collection_id' => $row ['collection_id'],
-					'title' => $row ['title'],
-					'author' => $row ['creator'],
-					'publisher' => $row ['publisher'],
+					'title' => urlencode($row ['title']),
+					'creator' => urlencode($row ['creator']),
+					'publisher' => urlencode($row ['publisher']),
 					'description' => urlencode ($row ['description']),
 					'language' => $row ['language'],
 					'date_issued' => $row ['date_issued'],
-					'type' => $row ['mimetype'],
+					'type' => $type,
 					// document (from bitstream)
 					'bitstream_id' => $bitstream_id,
-					'document_thumb_href' => $document_thumb_href,
-					'document_href' => $document_href,
+					'document_thumb_href' => urlencode($document_thumb_href),
+					'document_href' => urlencode($document_href),
 					'document_size' => $document_size 
 			);
 		}
@@ -83,7 +84,7 @@ else { // field not set in URL
 					'title' => urlencode ( $row ['label'] ), // url encoded!
 					'description' => urlencode ( $row ['description'] ), // url encoded!
 					'alias' => $row ['alias'],
-					'imageurl' => $row ['imageurl'] 
+					'imageurl' => urlencode($row ['imageurl'] )
 			);
 		}
 		
